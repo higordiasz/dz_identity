3e737792c039715e2abd424dd10f3912467a084c
Dias <higordiaszuqueto@gmail.com>
2022-03-11 14:00:05 -0300

    Command

--- skywalker.lua
+++ skywalker.lua
@@ -10,3 +10,15 @@ vRPclient = Tunnel.getInterface("vRP")
 src = {}
 Tunnel.bindInterface("dz_identity", src)
 vCLIENT = Tunnel.getInterface("dz_identity")
+
+-- ===================== Comandos =====================--
+
+RegisterCommand('identidade', function (source, args, rawCommand)
+    local source = source
+    local user_id = vRP.getUserId(source)
+    local identity = vRP.getUserIdentity(user_id)
+    local nplayer = vRPclient.getNearestPlayer(source, 3)
+    local nuser_id = vRP.getUserId(nplayer)
+    local identityu = vRP.getUserIdentitu(nuser_id)
+    
+end)
